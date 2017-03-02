e20ddbf351eef620bacb3f2c6693332a8009bd65
Hstamp <hattie.stamp@gmail.com>
2017-03-02 14:47:51 +0000

    Creating a text file with no HTML mark-up

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 
 html = open("eu_brexit_speech_corbin_2017.html")
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "lxml")
 text = soup.body.get_text("")
 
 
-print(text.encode('ascii', 'ignore'))
+with open("strip_brexit_speech.txt", "w") as f:
+    f.write(text.encode('utf-8'))
