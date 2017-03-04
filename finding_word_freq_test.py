5fd960a976011e9b043c3ecf8513d526fb8ba6bf
Hstamp <hattie.stamp@gmail.com>
2017-03-04 15:28:31 +0000

    Adding top 20 and deleting "the"
    To be continued...

--- finding_word_freq_test.py
+++ finding_word_freq_test.py
@@ -4,12 +4,11 @@ wordcount = collections.Counter()
 with open ("strip_brexit_speech.txt", "r") as speech_file:
     for line in speech_file:
         wordcount.update(line.split())
+    if "the" in wordcount:
+        del wordcount["the"] #delete word from counter - to be improved
 
 print 'Most common:'
-for letter, count in wordcount.most_common():
+for letter, count in wordcount.most_common(20): #showing top 20 words
     print '%s: %d' % (letter, count)
-
-#wordcount.iteritems():
-#    print k, v
 
 speech_file.close()
