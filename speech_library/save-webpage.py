aea418d4e8ee143cdad42e6dae707805931cc4d7
aasima <aasimapathan@Aasimas-MacBook-Air.local>
2017-03-04 18:42:10 +0000

    Fixed loop
    Loop downloads all 364 speeches in about a minute

--- speech_library/save-webpage.py
+++ speech_library/save-webpage.py
@@ -1,14 +1,16 @@
 # save-webpage.py
-import csv
 import urllib2
 
-#for loop to count all URLs up to no. 364
-for i in range(365):
-    url = 'http://www.britishpoliticalspeech.org/speech-archive.htm?speech=1'.format(i)
-#use urllib2 to read the webpage
+speech_num = 364
+
+while speech_num >= 1:
+
+    url = 'http://www.britishpoliticalspeech.org/speech-archive.htm?speech=%r' % speech_num
+
     response = urllib2.urlopen(url)
     webContent = response.read()
-#save this webContent to an html fale named after i
-    f = open(str(i) + '.html', 'w') #filename
+
+    f = open(str(speech_num) + '.html', 'w') #filename
     f.write(webContent)
     f.close
+    speech_num -= 1
