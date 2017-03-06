c48496de2876d109b8b769f7a756a25968657fb7
aasima <aasimapathan@Aasimas-MacBook-Air.local>
2017-03-06 18:06:27 +0000

    Added stopword list
    Added stopword list

--- finding_word_freq_test.py
+++ finding_word_freq_test.py
@@ -4,8 +4,13 @@ wordcount = collections.Counter()
 with open ("strip_brexit_speech.txt", "r") as speech_file:
     for line in speech_file:
         wordcount.update(line.split())
-    if "the" in wordcount:
-        del wordcount["the"] #delete word from counter - to be improved
+    wordlist = ["the", "a", "is", "an", "it", "in", "and", "to"]
+
+    for word in wordlist:
+        if word in wordcount:
+            del wordcount[word] #delete word from counter - to be improved
+    #if "a" in wordcount:
+        #del wordcount["a"]
 
 print 'Most common:'
 for letter, count in wordcount.most_common(20): #showing top 20 words
