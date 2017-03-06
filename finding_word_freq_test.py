f5ccecd973947ea50ec2bcdf6cc36151a80dad9e
Hstamp <hattie.stamp@gmail.com>
2017-03-06 19:21:12 +0000

    Adding Speech library and finding top 20 words
    Finding top 20 words in each speech .html file

--- finding_word_freq_test.py
+++ finding_word_freq_test.py
@@ -1,19 +1,25 @@
 import collections
+import os
 
-wordcount = collections.Counter()
-with open ("strip_brexit_speech.txt", "r") as speech_file:
-    for line in speech_file:
-        wordcount.update(line.split())
-    wordlist = ["the", "a", "is", "an", "it", "in", "and", "to"]
+os.chdir("C:\Users\H Stamp\Hstamp.github.io\speech_library")
+
+speech_number = 364
+
+while speech_number >= 1:
+
+    wordcount = collections.Counter()
+    with open (str(speech_number) + ".html", "r") as speech_file:
+        for line in speech_file:
+            wordcount.update(line.split())
+        wordlist = ["the", "a", "is", "an", "it", "in", "and", "to"]
 
     for word in wordlist:
         if word in wordcount:
-            del wordcount[word] #delete word from counter - to be improved
-    #if "a" in wordcount:
+            del wordcount[word] #if "a" in wordcount:
         #del wordcount["a"]
 
-print 'Most common:'
-for letter, count in wordcount.most_common(20): #showing top 20 words
-    print '%s: %d' % (letter, count)
-
-speech_file.close()
+    print 'Most common:'
+    for letter, count in wordcount.most_common(20): #showing top 20 words
+        print '%s: %d' % (letter, count)
+    speech_file.close()
+    speech_number -= 1
