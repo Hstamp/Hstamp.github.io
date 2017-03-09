230196a99d1a6f357b02fd35c984b9b171e1cee0
Hstamp <hattie.stamp@gmail.com>
2017-03-09 15:22:33 +0000

    Minor changes
    Added a few new words to Wordlist, changed counter for most_common to 10

--- finding_word_freq_test.py
+++ finding_word_freq_test.py
@@ -11,7 +11,7 @@ while speech_number >= 1:
     with open (str(speech_number) + ".html", "r") as speech_file:
         for line in speech_file:
             wordcount.update(line.split())
-        wordlist = ["were", "But", "should", "hoc", "ad", "one", "say", "such", "these", "upon", "when", "year", "<p><strong>The", "last", "other", "He", "can", "as", "he", "at", "has", "all", "what", "will", "may", "The", "or", "who", "if", "us", "from", "no", "It", "would", "been", "so", "<p>", "the", "a", "is", "an", "it", "in", "and", "to", "of", "but", "you", "And", "be", "are", "have", "by", "not", "must", "our", "I", "that", "type='text/css'", "of", "which", "that", "-", "this", "for", "we", "their", "they", "his", "on", "was", "with"]
+            wordlist = ["were", "But", "should", "</div>", "<div", "/>", "do", "<a", "do", "We", "while", "those", "very", "most", "am", "some", "into", "hoc", "ad", "one", "say", "such", "these", "upon", "when", "year", "<p><strong>The", "last", "other", "He", "can", "as", "he", "at", "has", "all", "what", "will", "may", "The", "or", "who", "if", "us", "from", "no", "It", "would", "been", "so", "<p>", "the", "a", "is", "an", "it", "in", "and", "to", "of", "but", "you", "And", "be", "are", "have", "by", "not", "must", "our", "I", "that", "type='text/css'", "of", "which", "that", "-", "this", "for", "we", "their", "they", "his", "on", "was", "with"]
 
     for word in wordlist:
         if word in wordcount:
@@ -20,7 +20,7 @@ while speech_number >= 1:
 
     with open("top_twenty_words.txt", "w") as f:
         f.write("Top 20 words from speech %s" % speech_number)
-        for letter, count in wordcount.most_common(20): #showing top 20 words
+        for letter, count in wordcount.most_common(10): #showing top 20 words
             f.write("%s: %d" % (letter, count))
 
     #with open("strip_brexit_speech.txt", "w") as f:
