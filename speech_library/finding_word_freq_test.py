6106062cf8514b6f324290ea52c45120840d1839
Hstamp <hattie.stamp@gmail.com>
2017-03-12 18:55:41 +0000

    Combining our scripts together!

--- speech_library/finding_word_freq_test.py
+++ speech_library/finding_word_freq_test.py
@@ -1,5 +1,20 @@
 import collections
 import os
+import urllib2
+
+speech_num = 364
+
+while speech_num >= 1:
+
+    url = 'http://www.britishpoliticalspeech.org/speech-archive.htm?speech=%r' % speech_num
+
+    response = urllib2.urlopen(url)
+    webContent = response.read()
+
+    f = open(str(speech_num) + '.html', 'w') #filename
+    f.write(webContent)
+    f.close
+    speech_num -= 1
 
 os.chdir("C:\Users\H Stamp\Documents\GitHub\Hstamp.github.io\speech_library")
 
